274c203985dcafbcc55a507f7a9aa2c8e333423d
Shayklos <shaymin1010@gmail.com>
2024-06-03 13:08:18 +0200

    Move example data into a different folder

--- sfa/shared.py
+++ sfa/shared.py
@@ -3,4 +3,6 @@ from pathlib import Path
 import pandas as pd
 
 app_dir = Path(__file__).parent
-tips = pd.read_csv(app_dir / "tips.csv")
+data_dir = app_dir.parent / 'example_data'
+
+tips = pd.read_csv(data_dir / "tips.csv")
