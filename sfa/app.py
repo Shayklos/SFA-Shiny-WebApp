d753910994249c3bd595b32f0a8e74853f58efd6
Shayklos <shaymin1010@gmail.com>
2024-06-03 23:38:02 +0200

    Add some webapp logic

--- sfa/app.py
+++ sfa/app.py
@@ -1,5 +1,8 @@
-from shiny.express import ui
+from shiny.express import ui, render, input
+
+
 from shared import data_dir, data_files
+import pandas as pd
 
 
 # ui.input_checkbox("show", "Show radio buttons", False)
@@ -13,11 +16,38 @@ from shared import data_dir, data_files
 # with ui.panel_conditional("input.show && input.radio === 'select'"):
 #     ui.input_select("select", None, ["A", "B", "C"])
 
-with ui.layout_sidebar(border=True):
-    with ui.sidebar(bg="#F5F5F5"):
-        ui.input_select("datafile_selection", "Choose the data", 
-            {
-                "Example files": {i: element for i, element in enumerate(data_files)},
-                "Personalized data": {"w":"Write my own data", "r":"Create random data", "u":"Upload file"}
-            }
-        )
+with ui.sidebar(bg="#F5F5F5"):
+    ui.input_select("datafile_selection", "Choose the data", 
+        {
+            "Example files": {f"example_datafile:{i}": element.name for i, element in enumerate(data_files)},
+            "Personalized data": {"w":"Write my own data", "r":"Create random data", "u":"Upload file"}
+        }
+    )
+    with ui.panel_conditional("input.datafile_selection.includes('example_datafile')"):
+        
+        ui.input_select("ineff_distr", "Distribution of the inefficiency term", ["A", "B", "C"])
+
+
+with ui.panel_conditional("!input.datafile_selection.includes('example_datafile')"):
+    "Not implemented"
+
+with ui.panel_conditional("input.datafile_selection.includes('example_datafile')"):
+    with ui.navset_pill(id="tab"):  
+        with ui.nav_panel("Data"):
+            with ui.card(full_screen=True):
+                ui.card_header("Data")
+
+                @render.data_frame
+                def table():
+                    if ':' not in (selection := input.datafile_selection.get()): 
+                        return
+                    index = int(selection.split(":")[-1])
+                    file = data_files[index]
+                    data = pd.read_csv(file)
+
+                    return render.DataGrid(data)
+
+        with ui.nav_panel("Result"):
+            @render.text
+            def f():
+                return input.ineff_distr
