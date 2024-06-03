6ea9b3193625a3e43f3e593a23b1f1cbaae09614
Shayklos <shaymin1010@gmail.com>
2024-06-03 14:19:12 +0200

    Select menu

--- sfa/app.py
+++ sfa/app.py
@@ -1,12 +1,23 @@
 from shiny.express import ui
+from shared import data_dir, data_files
 
-ui.input_checkbox("show", "Show radio buttons", False)
 
-with ui.panel_conditional("input.show"):
-    ui.input_radio_buttons("radio", "Choose ", ["slider", "select"])
+# ui.input_checkbox("show", "Show radio buttons", False)
 
-with ui.panel_conditional("input.show && input.radio === 'slider'"):
-    ui.input_slider("slider", None, min=0, max=100, value=50)
+# with ui.panel_conditional("input.show"):
+#     ui.input_radio_buttons("radio", "Choose ", ["slider", "select"])
 
-with ui.panel_conditional("input.show && input.radio === 'select'"):
-    ui.input_select("select", None, ["A", "B", "C"])
+# with ui.panel_conditional("input.show && input.radio === 'slider'"):
+#     ui.input_slider("slider", None, min=0, max=100, value=50)
+
+# with ui.panel_conditional("input.show && input.radio === 'select'"):
+#     ui.input_select("select", None, ["A", "B", "C"])
+
+with ui.layout_sidebar(border=True):
+    with ui.sidebar(bg="#F5F5F5"):
+        ui.input_select("datafile_selection", "Choose the data", 
+            {
+                "Example files": {i: element for i, element in enumerate(data_files)},
+                "Personalized data": {"w":"Write my own data", "r":"Create random data", "u":"Upload file"}
+            }
+        )
--- sfa/shared.py
+++ sfa/shared.py
@@ -4,5 +4,6 @@ import pandas as pd
 
 app_dir = Path(__file__).parent
 data_dir = app_dir.parent / 'example_data'
+data_files = [f.name for f in data_dir.glob('**/*') if f.is_file()]
 
 tips = pd.read_csv(data_dir / "tips.csv")
